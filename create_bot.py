e71fe8387a07dba3bc741d622f2a5954a94ad8d1
ChirkovRoman1984 <chirkovroman1984@gmail.com>
2022-12-09 19:27:25 +0400

    minus token

--- create_bot.py
+++ create_bot.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-API_TOKEN = '5366990934:AAFAnuzrrZQ5aUjBsjckOrEOiG5oREkCNBI'
+API_TOKEN = ''
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
